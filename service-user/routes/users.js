617f36e277000e2e993672910f322c7b9ca68046
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2022-02-02 16:20:10 +0700

    Feat( API get User)

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -4,5 +4,7 @@ const usersHandler = require("./handler/users");
 
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
+router.put("/:id", usersHandler.update);
+router.get("/:id", usersHandler.getUser);
 
 module.exports = router;
