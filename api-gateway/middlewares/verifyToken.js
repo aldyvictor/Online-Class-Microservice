60933f07bdcfd8f131c6b6bf5148caf5b0e86d0a
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2022-02-03 08:40:14 +0700

    Feat( Integeration API user update )

--- api-gateway/middlewares/verifyToken.js
+++ api-gateway/middlewares/verifyToken.js
@@ -3,14 +3,14 @@ const { JWT_SECRET } = process.env;
 
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
-  jwt.verify(token, JWT_SECRET, function (err, decode) {
+  jwt.verify(token, JWT_SECRET, function (err, decoded) {
     if (err) {
       return res.status(403).json({
         message: err.message,
       });
     }
 
-    req.user = decode;
+    req.user = decoded;
     return next();
   });
 };
