f0688d823bdc98939a81196c2a957c77fa8a40ec
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2022-02-08 16:43:46 +0700

    Feat( API create my courses )

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ImageCourseController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\MentorController;
+use App\Http\Controllers\MyCourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -48,3 +49,6 @@ Route::delete('lessons/{id}', [LessonController::class, 'destroy']);
 // Endpoint Image Course
 Route::post('image-courses', [ImageCourseController::class, 'create']);
 Route::delete('image-courses/{id}', [ImageCourseController::class, 'destroy']);
+
+// Endpoint My Course
+Route::post('/my-courses', [MyCourseController::class, 'create']);
